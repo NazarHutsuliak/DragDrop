ff801a6ea4c61483aa1adb8b2cc629636d8ac2a8
NHutsuliak <NHutsuliak@VINW0260>
2021-07-29 18:14:20 +0300

    Add project files.

---
+++ DragDrop/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using DragDrop.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DragDrop.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationContext _context;
+        IWebHostEnvironment _appEnvironment;
+        public HomeController(ApplicationContext context, IWebHostEnvironment appEnvironment)
+        {
+            _context = context;
+            _appEnvironment = appEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Files.ToList());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                // путь к папке Files
+                string path = "/Files/" + uploadedFile.FileName;
+                // сохраняем файл в папку Files в каталоге wwwroot
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+                _context.Files.Add(file);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
